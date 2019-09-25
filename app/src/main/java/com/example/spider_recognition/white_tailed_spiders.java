40fa760ea485bade52d2dfe3f694040b99a0058e
bweceo <32606742+bweceo@users.noreply.github.com>
2019-09-26 02:09:05 +1000

    first commit, tz

--- app/src/main/java/com/example/spider_recognition/white_tailed_spiders.java
+++ app/src/main/java/com/example/spider_recognition/white_tailed_spiders.java
@@ -10,16 +10,17 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import android.text.Html;
 
-public class redback_spider extends AppCompatActivity {
+
+public class white_tailed_spiders extends AppCompatActivity {
+
     @BindView(R.id.textView)
     TextView text;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_redback_spider);
+        setContentView(R.layout.activity_white_tailed_spiders);
         TextView mTextView = (TextView) findViewById(R.id.editText3);
-        mTextView.setText(Html.fromHtml(getResources().getString(R.string.redback_hazard_level)));
+        mTextView.setText(Html.fromHtml(getResources().getString(R.string.white_tailed_level)));
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
